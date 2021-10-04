c7b2dd069c0c9733d4033914a7569479c18edcd7
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-04 17:31:25 +1300

    add main activity page placeholder

--- view/src/main/java/se306p2/view/main/MainActivity.java
+++ view/src/main/java/se306p2/view/main/MainActivity.java
@@ -1,4 +1,37 @@
 package se306p2.view.main;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import se306p2.view.R;
+import se306p2.view.landingpage.LandingPageActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout splashLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        splashLayout = (RelativeLayout) findViewById(R.id.splash_layout);
+
+        splashLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LandingPageActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
