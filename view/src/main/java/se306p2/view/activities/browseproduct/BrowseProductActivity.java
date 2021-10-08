114c2823e775daa64f818a45486b476e823e5893
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-08 21:00:35 +1300

    add base for browse product page

--- view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
+++ view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
@@ -1,4 +1,28 @@
 package se306p2.view.activities.browseproduct;
 
-public class BrowseProductActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import se306p2.view.R;
+
+public class BrowseProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_products_view);
+
+        Spinner spinner = (Spinner) findViewById(R.id.priceFilter);
+        String[] years = {"$0 - $49","$50 - $149","$150 - $299","$299+"};
+        ArrayAdapter<CharSequence> langAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_text, years );
+        langAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinner.setAdapter(langAdapter);
+
+    }
+
 }
