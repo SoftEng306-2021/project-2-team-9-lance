637d1ac196c35f3a5423f5ef8a25f9b282725e59
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-08 21:36:16 +1300

    finish rough implementation of browse products UI

--- view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
+++ view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
@@ -2,15 +2,27 @@ package se306p2.view.activities.browseproduct;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import se306p2.domain.interfaces.entity.IProduct;
 import se306p2.view.R;
+import se306p2.view.common.adapters.ProductItemRecyclerViewAdapter;
+import se306p2.view.common.placeholders.PlaceholderGenerator;
 
 public class BrowseProductActivity extends AppCompatActivity {
+    private static final String TAG = "BrowseProductActivity";
+
+    private List<IProduct> productList = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +40,22 @@ public class BrowseProductActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> brandAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_text, years );
         brandAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         brandSpinner.setAdapter(brandAdapter);
+
+        productList.addAll(PlaceholderGenerator.getProducts(20));
+
+        initProductsRecyclerView();
+    }
+
+    private void initProductsRecyclerView() {
+        Log.d(TAG, "initFeaturedListRecyclerView entered");
+
+        RecyclerView recyclerView = findViewById(R.id.browse_products_list);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+
+        ProductItemRecyclerViewAdapter adapter = new ProductItemRecyclerViewAdapter(this, productList);
+        recyclerView.setAdapter(adapter);
     }
 
 }
