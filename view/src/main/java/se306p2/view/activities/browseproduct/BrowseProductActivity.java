2d1575297600605031a500ac409bf409da269dcd
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-08 21:16:20 +1300

    finalise filter UI

--- view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
+++ view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductActivity.java
@@ -17,12 +17,17 @@ public class BrowseProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse_products_view);
 
-        Spinner spinner = (Spinner) findViewById(R.id.priceFilter);
-        String[] years = {"$0 - $49","$50 - $149","$150 - $299","$299+"};
-        ArrayAdapter<CharSequence> langAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_text, years );
-        langAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        spinner.setAdapter(langAdapter);
+        Spinner priceSpinner = (Spinner) findViewById(R.id.priceFilter);
+        String[] prices = {"$0 - $49","$50 - $149","$150 - $299","$299+"};
+        ArrayAdapter<CharSequence> priceAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_text, prices );
+        priceAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        priceSpinner.setAdapter(priceAdapter);
 
+        Spinner brandSpinner = (Spinner) findViewById(R.id.brandFilter);
+        String[] years = {"Apple", "Pear", "Watermelon"};
+        ArrayAdapter<CharSequence> brandAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_text, years );
+        brandAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        brandSpinner.setAdapter(brandAdapter);
     }
 
 }
