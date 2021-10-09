d3f264dc3b2c11c27164fb93010d3e1316d10ab1
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-10 03:07:51 +1300

    remove benefitId

--- model/src/main/java/se306p2/model/entities/Benefit.java
+++ model/src/main/java/se306p2/model/entities/Benefit.java
@@ -4,13 +4,11 @@ import se306p2.domain.interfaces.entity.IBenefit;
 
 public class Benefit implements IBenefit {
     private String id;
-    private String benefitId;
     private String name;
     private String imageURI;
 
     public Benefit(String id, String benefitId,String name, String imageURI) {
         this.id = id;
-        this.benefitId = benefitId;
         this.name = name;
         this.imageURI = imageURI;
     }
--- view/src/main/java/se306p2/view/common/placeholders/placeholderEntities/PlaceholderBenefit.java
+++ view/src/main/java/se306p2/view/common/placeholders/placeholderEntities/PlaceholderBenefit.java
@@ -5,13 +5,11 @@ import se306p2.domain.interfaces.entity.IBenefit;
 public class PlaceholderBenefit implements IBenefit {
 
     private String id;
-    private String benefitId;
     private String name;
     private String imageURI;
 
     public PlaceholderBenefit(String id, String benefitId,String name, String imageURI) {
         this.id = id;
-        this.benefitId = benefitId;
         this.name = name;
         this.imageURI = imageURI;
     }
