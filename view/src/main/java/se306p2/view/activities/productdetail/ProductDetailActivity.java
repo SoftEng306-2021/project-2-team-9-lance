7e2f7623cbd7d0426d811855fc38d1d5d169456f
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-10 03:56:08 +1300

    finalise animated collapsibles for product details

--- view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
+++ view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
@@ -20,6 +20,8 @@ import com.google.android.flexbox.FlexWrap;
 import com.google.android.flexbox.FlexboxLayoutManager;
 import com.google.android.flexbox.JustifyContent;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 import se306p2.domain.interfaces.entity.IBenefit;
@@ -31,7 +33,6 @@ import se306p2.view.common.placeholders.PlaceholderGenerator;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
-    //to be moved to ViewModel
     private IProduct product;
     private List<IBenefit> benefits;
 
@@ -39,7 +40,6 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_details_view);
-
 
         getData();
 
@@ -63,31 +63,33 @@ public class ProductDetailActivity extends AppCompatActivity {
     }
 
     private void initProductInfo() {
-        TextView brandName, productName, slogan, description, priceDollars, priceCents;
+        TextView brandName, productName, slogan, priceDollars, priceCents;
 
         brandName = (TextView) findViewById(R.id.product_details_brand);
         productName = (TextView) findViewById(R.id.product_details_name);
         slogan = (TextView) findViewById(R.id.product_details_slogan);
-        description = (TextView) findViewById(R.id.product_details_description);
         priceDollars = (TextView) findViewById(R.id.product_details_cents);
         priceCents = (TextView) findViewById(R.id.product_details_cents);
 
         brandName.setText(product.getBrandName());
         productName.setText(product.getName());
         slogan.setText(product.getSlogan());
-        description.setText(product.getDetails());
 
         String[] formattedPrice = DisplayDataFormatter.formatPriceData(product.getPrice());
-
         priceDollars.setText(formattedPrice[0]);
         priceCents.setText(formattedPrice[1]);
     }
 
     private void initDetails() {
+        TextView description = (TextView) findViewById(R.id.product_details_description);
+        description.setText(product.getDetails());
+
         LinearLayoutCompat detailsTitle = (LinearLayoutCompat) findViewById(R.id.product_details_details_title);
         LinearLayoutCompat detailsContent = (LinearLayoutCompat) findViewById(R.id.product_details_details_content);
 
         ImageView chevronIcon = (ImageView)findViewById(R.id.product_details_details_chevron);
+
+        hideSection(detailsContent, chevronIcon);
 
         detailsTitle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,6 +105,8 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         ImageView chevronIcon = (ImageView)findViewById(R.id.product_details_benefits_chevron);
 
+        hideSection(benefitsContent, chevronIcon);
+
         benefitsTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,7 +118,7 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
         layoutManager.setFlexDirection(FlexDirection.ROW);
-        layoutManager.setJustifyContent(JustifyContent.SPACE_BETWEEN);
+        layoutManager.setJustifyContent(JustifyContent.FLEX_START);
         layoutManager.setFlexWrap(FlexWrap.WRAP);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -124,10 +128,15 @@ public class ProductDetailActivity extends AppCompatActivity {
     }
 
     private void initIngredients() {
+        TextView ingredients = (TextView) findViewById(R.id.product_details_ingredients);
+        ingredients.setText(product.getIngredients());
+
         LinearLayoutCompat ingredientsTitle = (LinearLayoutCompat) findViewById(R.id.product_details_ingredients_title);
         LinearLayoutCompat ingredientsContent = (LinearLayoutCompat) findViewById(R.id.product_details_ingredients_content);
 
         ImageView chevronIcon = (ImageView)findViewById(R.id.product_details_ingredients_chevron);
+
+        hideSection(ingredientsContent, chevronIcon);
 
         ingredientsTitle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,20 +148,27 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     private void toggleShowSection(View toggleView, ImageView chevron) {
         if (toggleView.isShown()) {
-            toggleView.setVisibility(View.GONE);
-            RotateAnimation r = new RotateAnimation(0.0f, 90.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            r.setDuration(300);
-            r.setRepeatCount(0);
-            r.setFillAfter(true);
-            chevron.startAnimation(r);
+            hideSection(toggleView,  chevron);
         } else {
-            toggleView.setVisibility(View.VISIBLE);
-            RotateAnimation r = new RotateAnimation(90.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            r.setDuration(300);
-            r.setRepeatCount(0);
-            r.setFillAfter(true);
-            chevron.startAnimation(r);
+            showSection( toggleView,  chevron);
         }
     }
 
+    private void hideSection(View toggleView, ImageView chevron) {
+        toggleView.setVisibility(View.GONE);
+        RotateAnimation r = new RotateAnimation(0.0f, 90.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        r.setDuration(300);
+        r.setRepeatCount(0);
+        r.setFillAfter(true);
+        chevron.startAnimation(r);
+    }
+
+    private void showSection(View toggleView, ImageView chevron) {
+        toggleView.setVisibility(View.VISIBLE);
+        RotateAnimation r = new RotateAnimation(90.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        r.setDuration(300);
+        r.setRepeatCount(0);
+        r.setFillAfter(true);
+        chevron.startAnimation(r);
+    }
 }
