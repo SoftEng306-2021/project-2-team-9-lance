367249ca616bf7f98283b52315d9912e19658d83
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-10 01:27:16 +1300

    spent waaay too long animation this (again)

--- view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
+++ view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
@@ -1,10 +1,15 @@
 package se306p2.view.activities.productdetail;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.LayoutTransition;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import se306p2.domain.interfaces.entity.IProduct;
 import se306p2.view.R;
@@ -16,11 +21,11 @@ public class ProductDetailActivity extends AppCompatActivity {
     //to be moved to ViewModel
     private IProduct product;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_details_view);
+
 
         getData();
 
@@ -50,6 +55,46 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         priceDollars.setText(formattedPrice[0]);
         priceCents.setText(formattedPrice[1]);
+
+        LinearLayoutCompat detailsTitle = (LinearLayoutCompat) findViewById(R.id.product_details_details_title);
+        LinearLayoutCompat detailsContent = (LinearLayoutCompat) findViewById(R.id.product_details_details_content);
+
+        LinearLayoutCompat rootLinearLayout = (LinearLayoutCompat)findViewById(R.id.product_details_container);
+
+        detailsTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutTransition layoutTransition = rootLinearLayout.getLayoutTransition();
+                layoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+                if (detailsContent.isShown()) {
+//                    detailsContent.setAlpha(1.0f);
+//
+//// Start the animation
+//                    detailsContent.animate()
+//                            .translationY(-detailsContent.getHeight())
+//                            .alpha(0.0f)
+//                            .setListener(new AnimatorListenerAdapter() {
+//                                @Override
+//                                public void onAnimationEnd(Animator animation) {
+//                                    super.onAnimationEnd(animation);
+//                                    detailsContent.setVisibility(View.GONE);
+//                                }
+//                            });
+
+                    detailsContent.setVisibility(view.GONE);
+                } else {
+                    detailsContent.setVisibility(View.VISIBLE);
+//                    detailsContent.setAlpha(0.0f);
+//
+//// Start the animation
+//                    detailsContent.animate()
+//                            .translationY(0)
+//                            .alpha(1.0f)
+//                            .setListener(null);
+                }
+            }
+        });
+
 
     }
 
