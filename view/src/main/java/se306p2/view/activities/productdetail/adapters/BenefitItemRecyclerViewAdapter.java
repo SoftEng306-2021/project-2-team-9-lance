d35a0cd757df309b81f0fb0f44b2b2f3fc058f95
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-10 02:48:53 +1300

    create recycler view adapter for benefits and fix category adapter issue

--- view/src/main/java/se306p2/view/activities/productdetail/adapters/BenefitItemRecyclerViewAdapter.java
+++ view/src/main/java/se306p2/view/activities/productdetail/adapters/BenefitItemRecyclerViewAdapter.java
@@ -1,4 +1,62 @@
 package se306p2.view.activities.productdetail.adapters;
 
-public class BenefitItemRecyclerViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import se306p2.domain.interfaces.entity.IBenefit;
+import se306p2.view.R;
+
+public class BenefitItemRecyclerViewAdapter extends RecyclerView.Adapter<BenefitItemRecyclerViewAdapter.ViewHolder> {
+
+
+    private Context context;
+    private List<IBenefit> benefits;
+
+    public BenefitItemRecyclerViewAdapter(Context context, List<IBenefit> benefits) {
+        this.context = context;
+        this.benefits = benefits;
+    }
+    @NonNull
+    @Override
+    public BenefitItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.benefit_listitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BenefitItemRecyclerViewAdapter.ViewHolder holder, int position) {
+        Glide.with(context)
+                .asBitmap()
+                .load(benefits.get(position).getImageURI())
+                .into(holder.benefitImage);
+
+        holder.benefitName.setText(benefits.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return benefits.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView benefitImage;
+        TextView benefitName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            benefitImage = itemView.findViewById(R.id.benefit_image);
+            benefitName = itemView.findViewById(R.id.benefit_name);
+        }
+    }
 }
