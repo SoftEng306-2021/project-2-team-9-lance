830561a7db32a5a35ec43362c36e124a41104314
ZHENK <chengzhenyang@gmail.com>
2021-10-10 07:38:53 +1300

    Attempt to implement UserRepository

--- model/src/main/java/se306p2/model/repository/UserRepository.java
+++ model/src/main/java/se306p2/model/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package se306p2.model.repository;
 
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import se306p2.domain.interfaces.repositories.IUserRepository;
 
@@ -11,31 +20,77 @@ public class UserRepository implements IUserRepository {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-    private UserRepository instance;
+    private static UserRepository instance;
 
-
-    @Override
-    public IUserRepository getInstance() {
-        throw new UnsupportedOperationException();
+    private UserRepository(FirebaseFirestore firestore, FirebaseAuth auth) {
+        this.db = firestore;
+        this.mAuth = auth;
     }
 
-    @Override
+    public static IUserRepository getInstance() {
+        if (instance == null) {
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            instance = new UserRepository(firestore, auth);
+        }
+        return instance;
+    }
+
     public String getCurrentUserId() {
-        throw new UnsupportedOperationException();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            return null;
+        }
+        return currentUser.getUid();
     }
 
-    @Override
     public String signInAnonymously() {
-        throw new UnsupportedOperationException();
+        try {
+            AuthResult result = Tasks.await(mAuth.signInAnonymously());
+            return result.getUser().getUid();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    @Override
     public Set<String> favourites() {
-        throw new UnsupportedOperationException();
+        String userId = getCurrentUserId();
+        if (userId == null) {
+            return null;
+        }
+
+        try {
+            DocumentSnapshot snapshot = Tasks.await(db.collection("user").document(userId).get());
+            List<DocumentReference> favouriteList = (List<DocumentReference>) snapshot.get("favourites");
+
+            return new HashSet<>(favouriteList.stream().map(s -> s.getId()).collect(Collectors.toList()));
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    @Override
     public Set<String> favourite(String productId) {
-        throw new UnsupportedOperationException();
+        String userId = getCurrentUserId();
+        if (userId == null) {
+            return null;
+        }
+
+        try {
+            DocumentSnapshot snapshot = Tasks.await(db.collection("user").document(userId).get());
+            List<DocumentReference> favouriteList = (List<DocumentReference>) snapshot.get("favourites");
+
+            favouriteList.add(db.collection("product").document(productId));
+
+            Tasks.await(db.collection("user")
+                    .document(userId)
+                    .update("favourites", favouriteList)
+            );
+            return favourites();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
