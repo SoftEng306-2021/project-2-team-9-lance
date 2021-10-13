14f901002f533d441910c9d5cfdf94981d20757a
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-14 02:03:57 +1300

    test calling use cases

--- view/src/main/java/se306p2/view/activities/main/MainActivity.java
+++ view/src/main/java/se306p2/view/activities/main/MainActivity.java
@@ -8,6 +8,13 @@ import android.widget.RelativeLayout;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import se306p2.domain.RepositoryRouter;
+import se306p2.domain.usecase.GetCurrentUserIdUseCase;
+import se306p2.domain.usecase.SignInAnonymouslyUseCase;
+import se306p2.model.repository.BrandRepository;
+import se306p2.model.repository.CategoryRepository;
+import se306p2.model.repository.ProductRepository;
+import se306p2.model.repository.UserRepository;
 import se306p2.view.R;
 import se306p2.view.activities.landingpage.LandingPageActivity;
 
@@ -22,13 +29,19 @@ public class MainActivity extends AppCompatActivity {
 
         splashLayout = (RelativeLayout) findViewById(R.id.splash_layout);
 
-        splashLayout.setOnClickListener(new View.OnClickListener(){
+        splashLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LandingPageActivity.class);
                 startActivity(intent);
             }
         });
+
+        RepositoryRouter.init(BrandRepository.getInstance(), CategoryRepository.getInstance(), ProductRepository.getInstance(), null, UserRepository.getInstance());
+        String testGetUserId = new GetCurrentUserIdUseCase().getCurrentUserId();
+        String testSignIn = new SignInAnonymouslyUseCase().signInAnonymously();
+        System.out.println(testGetUserId);
+        System.out.println(testSignIn);
     }
 
 
