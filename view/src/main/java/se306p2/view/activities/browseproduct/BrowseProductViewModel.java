2a0b3127600c84be032ed14d137b588fdc23b5a0
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-14 01:49:49 +1300

    android hard me dumb

---
+++ view/src/main/java/se306p2/view/activities/browseproduct/BrowseProductViewModel.java
@@ -0,0 +1,48 @@
+package se306p2.view.activities.browseproduct;
+
+import android.app.Application;
+
+import androidx.lifecycle.ViewModel;
+
+import se306p2.domain.interfaces.entity.ICategory;
+import se306p2.domain.interfaces.usecase.IGetBrandsUseCase;
+import se306p2.domain.interfaces.usecase.IGetMaxPriceUseCase;
+import se306p2.domain.interfaces.usecase.IGetMinPriceUseCase;
+import se306p2.domain.interfaces.usecase.IGetProductVersionsUseCase;
+import se306p2.domain.interfaces.usecase.IGetProductsByFilterUseCase;
+import se306p2.domain.usecase.GetBrandsUseCase;
+import se306p2.domain.usecase.GetMaxPriceUseCase;
+import se306p2.domain.usecase.GetMinPriceUseCase;
+import se306p2.domain.usecase.GetProductsByFilterUseCase;
+
+public class BrowseProductViewModel extends ViewModel {
+
+    IGetMaxPriceUseCase getMaxPriceUseCase = new GetMaxPriceUseCase();
+    IGetMinPriceUseCase getMinPriceUseCase = new GetMinPriceUseCase();
+    IGetProductsByFilterUseCase getProductsByFilterUseCase = new GetProductsByFilterUseCase();
+    IGetBrandsUseCase getBrandsUseCase = new GetBrandsUseCase();
+
+    String categoryId;
+    
+
+
+    public BrowseProductViewModel(Application application, ICategory category) {
+
+
+    }
+
+    public void loadPageData() {
+        //load products
+    }
+
+    public void loadProducts(String categoryId, ) {
+        //load produx
+
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+
+}
