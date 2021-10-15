eb8ec93c543628c5b8069280e75ad795d07fda1c
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-16 00:56:29 +1300

    update logic to load benefits and product

--- view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
+++ view/src/main/java/se306p2/view/activities/productdetail/ProductDetailActivity.java
@@ -15,6 +15,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.flexbox.FlexDirection;
@@ -30,6 +31,7 @@ import se306p2.domain.interfaces.entity.IBenefit;
 import se306p2.domain.interfaces.entity.IProduct;
 import se306p2.view.R;
 import se306p2.view.activities.productdetail.adapters.BenefitItemRecyclerViewAdapter;
+import se306p2.view.common.adapters.ProductItemRecyclerViewAdapter;
 import se306p2.view.common.helper.DisplayDataFormatter;
 
 import android.os.Bundle;
@@ -42,9 +44,10 @@ public class ProductDetailActivity extends AppCompatActivity {
     private String productId;
 
     private ProductDetailViewModel viewModel;
-    
-    TextView brandName, productName, slogan, priceDollars, priceCents;
 
+    private TextView brandName, productName, priceDollars, priceCents;
+    private TextView slogan, description;
+    private TextView ingredients;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -74,25 +77,28 @@ public class ProductDetailActivity extends AppCompatActivity {
     private void initProductInfo() {
         brandName = (TextView) findViewById(R.id.product_details_brand);
         productName = (TextView) findViewById(R.id.product_details_name);
-        slogan = (TextView) findViewById(R.id.product_details_slogan);
         priceDollars = (TextView) findViewById(R.id.product_details_cents);
         priceCents = (TextView) findViewById(R.id.product_details_cents);
 
-        viewModel.getProduct().
+        viewModel.getProduct().observe(this, observedProduct -> {
+            brandName.setText(observedProduct.getBrandName());
+            productName.setText(observedProduct.getName());
 
+            String[] formattedPrice = DisplayDataFormatter.formatPriceData(observedProduct.getPrice());
 
-        brandName.setText(product.getBrandName());
-        productName.setText(product.getName());
-        slogan.setText(product.getSlogan());
-
-        String[] formattedPrice = DisplayDataFormatter.formatPriceData(product.getPrice());
-        priceDollars.setText(formattedPrice[0]);
-        priceCents.setText(formattedPrice[1]);
+            priceDollars.setText(formattedPrice[0]);
+            priceCents.setText(formattedPrice[1]);
+        });
     }
 
     private void initDetails() {
-        TextView description = (TextView) findViewById(R.id.product_details_description);
-        description.setText(product.getDetails());
+        slogan = (TextView) findViewById(R.id.product_details_slogan);
+        description = (TextView) findViewById(R.id.product_details_description);
+
+        viewModel.getProduct().observe(this, observedProduct -> {
+            slogan.setText(observedProduct.getSlogan());
+            description.setText(observedProduct.getDetails());
+        });
 
         LinearLayoutCompat detailsTitle = (LinearLayoutCompat) findViewById(R.id.product_details_details_title);
         LinearLayoutCompat detailsContent = (LinearLayoutCompat) findViewById(R.id.product_details_details_content);
@@ -118,22 +124,29 @@ public class ProductDetailActivity extends AppCompatActivity {
                 toggleShowSection(benefitsContent, chevronIcon);
         });
 
+
         RecyclerView recyclerView = findViewById(R.id.product_details_benefits);
 
-        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
-        layoutManager.setFlexDirection(FlexDirection.ROW);
-        layoutManager.setJustifyContent(JustifyContent.FLEX_START);
-        layoutManager.setFlexWrap(FlexWrap.WRAP);
-        recyclerView.setLayoutManager(layoutManager);
+        viewModel.getBenefits().observe(this, observedBenefits -> {
+            BenefitItemRecyclerViewAdapter adapter = new BenefitItemRecyclerViewAdapter(this, observedBenefits);
 
-        BenefitItemRecyclerViewAdapter adapter = new BenefitItemRecyclerViewAdapter(this, benefits);
-        recyclerView.setAdapter(adapter);
+            FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
+            layoutManager.setFlexDirection(FlexDirection.ROW);
+            layoutManager.setJustifyContent(JustifyContent.FLEX_START);
+            layoutManager.setFlexWrap(FlexWrap.WRAP);
+            recyclerView.setLayoutManager(layoutManager);
+
+            recyclerView.setAdapter(adapter);
+        });
 
     }
 
     private void initIngredients() {
-        TextView ingredients = (TextView) findViewById(R.id.product_details_ingredients);
-        ingredients.setText(product.getIngredients());
+        ingredients = (TextView) findViewById(R.id.product_details_ingredients);
+
+        viewModel.getProduct().observe(this, observedProduct -> {
+            ingredients.setText(observedProduct.getIngredients());
+        });
 
         LinearLayoutCompat ingredientsTitle = (LinearLayoutCompat) findViewById(R.id.product_details_ingredients_title);
         LinearLayoutCompat ingredientsContent = (LinearLayoutCompat) findViewById(R.id.product_details_ingredients_content);
--- view/src/main/java/se306p2/view/activities/productdetail/ProductDetailViewModel.java
+++ view/src/main/java/se306p2/view/activities/productdetail/ProductDetailViewModel.java
@@ -1,5 +1,6 @@
 package se306p2.view.activities.productdetail;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -8,11 +9,14 @@ import java.util.List;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.observers.DisposableSingleObserver;
+import se306p2.domain.interfaces.entity.IBenefit;
 import se306p2.domain.interfaces.entity.IProduct;
 import se306p2.domain.interfaces.entity.IProductVersion;
+import se306p2.domain.interfaces.usecase.IGetBenefitsUseCase;
 import se306p2.domain.interfaces.usecase.IGetFavouritesUseCase;
 import se306p2.domain.interfaces.usecase.IGetProductUseCase;
 import se306p2.domain.interfaces.usecase.IGetProductVersionsUseCase;
+import se306p2.domain.usecase.GetBenefitsUseCase;
 import se306p2.domain.usecase.GetFavouritesUseCase;
 import se306p2.domain.usecase.GetProductUseCase;
 import se306p2.domain.usecase.GetProductVersionsUseCase;
@@ -24,32 +28,38 @@ public class ProductDetailViewModel extends ViewModel {
     private IGetProductUseCase getProductUseCase;
     private IGetProductVersionsUseCase getProductVersionsUseCase;
     private IGetFavouritesUseCase getFavouritesUseCase;
+    private IGetBenefitsUseCase getBenefitsUseCase;
 
     private String productId;
 
     private MutableLiveData<IProduct> product = new MutableLiveData<>();
+    private MutableLiveData<List<IBenefit>> benefits = new MutableLiveData<>();
     private MutableLiveData<List<IProductVersion>> productVersions = new MutableLiveData<>();
 
     public ProductDetailViewModel() {
         this.getProductUseCase = new GetProductUseCase();
         this.getProductVersionsUseCase = new GetProductVersionsUseCase();
         this.getFavouritesUseCase = new GetFavouritesUseCase();
+        this.getBenefitsUseCase = new GetBenefitsUseCase();
     }
 
     public ProductDetailViewModel(
             IGetProductUseCase getProductUseCase,
             IGetProductVersionsUseCase getProductVersionsUseCase,
-            IGetFavouritesUseCase getFavouritesUseCase
+            IGetFavouritesUseCase getFavouritesUseCase,
+            IGetBenefitsUseCase getBenefitsUseCase
     ) {
         this.getProductUseCase = getProductUseCase;
         this.getProductVersionsUseCase = getProductVersionsUseCase;
         this.getFavouritesUseCase = getFavouritesUseCase;
+        this.getBenefitsUseCase = getBenefitsUseCase;
     }
 
     public void init(String productId) {
         this.productId = productId;
 
         loadProduct();
+        loadBenefits();
     }
 
     private void loadProduct() {
@@ -68,10 +78,29 @@ public class ProductDetailViewModel extends ViewModel {
         }));
     }
 
-    public IProduct getProduct() {
-        return product.getValue();
+    private void loadBenefits() {
+        Single<List<IBenefit>> benefitsSingle = getBenefitsUseCase.getBenefits(productId);
+        this.disposables.add(benefitsSingle.subscribeWith(new DisposableSingleObserver<List<IBenefit>>() {
+            @Override
+            public void onSuccess(List<IBenefit> retrievedBenefit) {
+                benefits.postValue(retrievedBenefit);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                //Handle error
+            }
+        }));
     }
 
+    public LiveData<IProduct> getProduct() {
+        return product;
+    }
+
+    public LiveData<List<IBenefit>> getBenefits() {
+        return benefits;
+    }
 
     public void dispose() {
         this.disposables.dispose();
