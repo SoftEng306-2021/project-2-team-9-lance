ae8eb966fd6a85e313687e12290a04e15f749d78
ZHENK <chengzhenyang@gmail.com>
2021-10-15 13:37:16 +1300

    Create node scripts to insert data into Firestore

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+# Firestore Insert Data Scripts
+
+The following are scripts to insert data into our firestore database.
+
---
+++ scripts/categories.js
@@ -0,0 +1,59 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const GOOGLE_STORAGE =
+  "https://storage.googleapis.com/se306-project-2-team-9.appspot.com";
+
+const categories = [
+  {
+    id: "gxcn5w7AsbIgFLLzb9nW",
+    name: "Makeup",
+    image: `${GOOGLE_STORAGE}/categories/makeup.png`,
+  },
+  {
+    id: "AZc4nze30eOCxjGmMiVS",
+    name: "Skin Care",
+    image: `${GOOGLE_STORAGE}/categories/skincare.png`,
+  },
+  {
+    id: "ZXqgovMgJ666L1UaBjiL",
+    name: "Hair Care",
+    image: `${GOOGLE_STORAGE}/categories/haircare.png`,
+  },
+  {
+    id: "xBeOpIqRGbLorhAgYskL",
+    name: "Body Care",
+    image: `${GOOGLE_STORAGE}/categories/bodycare.png`,
+  },
+  {
+    id: "TMQjHswd34pclUuFNeT3",
+    name: "Fragrance",
+    image: `${GOOGLE_STORAGE}/categories/fragrance.png`,
+  },
+  {
+    id: "dGvaSxSK2eSVju1PdiMs",
+    name: "Accessories",
+    image: `${GOOGLE_STORAGE}/categories/accessories.png`,
+  },
+];
+
+const init = async () => {
+  await Promise.all(
+    categories.map(async (category) => {
+      const data = {
+        name: category.name,
+        image: category.image,
+      };
+
+      return await db.collection("categories").doc(category.id).set(data);
+    })
+  );
+};
+
+init();
