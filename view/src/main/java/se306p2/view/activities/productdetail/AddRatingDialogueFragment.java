12511e72f6dc287557ef6108aca1ab9e6faccc3e
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-10-18 23:55:20 +1300

    forgot to commit the actual fragment

---
+++ view/src/main/java/se306p2/view/activities/productdetail/AddRatingDialogueFragment.java
@@ -0,0 +1,67 @@
+package se306p2.view.activities.productdetail;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import se306p2.view.R;
+
+public class AddRatingDialogueFragment extends DialogFragment {
+
+    ProductDetailViewModel viewModel;
+
+    LinearLayoutCompat starsContainer;
+    Button doneButton;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.add_review_fragment, container, false);
+
+
+        viewModel = new ViewModelProvider(requireActivity()).get(ProductDetailViewModel.class);
+
+        starsContainer = rootView.findViewById(R.id.add_review_stars_container);
+        doneButton = rootView.findViewById(R.id.add_review_done);
+
+        viewModel.getGivenRating().observe(getActivity(), observedGivenRating -> {
+            starsContainer.removeAllViews();
+
+            if (observedGivenRating == null || observedGivenRating == 0) {
+                doneButton.setEnabled(false);
+                for (int i = 0; i < 5; i++) {
+                    ImageView star = new ImageView(getActivity().getApplicationContext());
+                    star.setImageDrawable(getActivity().getDrawable(R.drawable.ic_star_large_outline));
+
+                    star.setOnClickListener(e -> {
+                        viewModel.giveRating(i + 1);
+                    });
+
+                    starsContainer.addView(star);
+                }
+            } else {
+                doneButton.setEnabled(true);
+
+                for (int i = 0; i < 5; i++) {
+                    ImageView star = new ImageView(getActivity().getApplicationContext());
+
+                    if (i < observedGivenRating) {
+                        star.setImageDrawable(getActivity().getDrawable(R.drawable.ic_star_large_filled));
+                    } else {
+                        star.setImageDrawable(getActivity().getDrawable(R.drawable.ic_star_large_outline));
+                    }
+
+                    starsContainer.addView(star);
+                }
+            }
+        });
+    }
+
+}
