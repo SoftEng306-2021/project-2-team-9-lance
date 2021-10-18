e2e5f552f2109b1c9ab8a9f1877949dd01e37082
ZHENK <chengzhenyang@gmail.com>
2021-10-19 00:29:15 +1300

    Add and remove rating useCases (#36)

---
+++ domain/src/main/java/se306p2/domain/usecase/AddRatingUseCase.java
@@ -0,0 +1,23 @@
+package se306p2.domain.usecase;
+
+import io.reactivex.rxjava3.core.Single;
+import se306p2.domain.RepositoryRouter;
+import se306p2.domain.interfaces.entity.IRating;
+import se306p2.domain.interfaces.usecase.IAddRatingUseCase;
+
+public class AddRatingUseCase implements IAddRatingUseCase {
+    public Single<IRating> addRating(String productId, String userId, Integer addedRating) {
+        return Single.create(emitter -> {
+            try {
+                IRating rating = RepositoryRouter.getRatingRepository().addRating(productId, userId, addedRating);
+                if (rating == null) {
+                    emitter.onError(new NullPointerException());
+                    return;
+                }
+                emitter.onSuccess(rating);
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+    }
+}
---
+++ domain/src/main/java/se306p2/domain/usecase/RemoveRatingUseCase.java
@@ -0,0 +1,23 @@
+package se306p2.domain.usecase;
+
+import io.reactivex.rxjava3.core.Single;
+import se306p2.domain.RepositoryRouter;
+import se306p2.domain.interfaces.entity.IRating;
+import se306p2.domain.interfaces.usecase.IRemoveRatingUseCase;
+
+public class RemoveRatingUseCase implements IRemoveRatingUseCase {
+    public Single<IRating> removeRating(String productId, String userId) {
+        return Single.create(emitter -> {
+            try {
+                IRating rating = RepositoryRouter.getRatingRepository().removeRating(productId, userId);
+                if (rating == null) {
+                    emitter.onError(new NullPointerException());
+                    return;
+                }
+                emitter.onSuccess(rating);
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+    }
+}
